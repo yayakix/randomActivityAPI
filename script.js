9923d8f55d3af866a60fc37148a5eff258f09dd7
Iyana Marquez <yayakix@gmail.com>
2022-05-09 10:54:08 -0400

    completed price logic

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ $("#randomActivity").click(function () {
       console.log(data.price);
       $("#typeData").text(`Type: ${data.type}`);
       $("#activityData").text(`Activity: ${data.activity}`);
-    //   format price 
+      //   format price
       if (data.price == 0.0) {
         $("#priceData").text(`Price: free`);
       } else if (data.price <= 0.3) {
@@ -30,20 +30,42 @@ $("#randomActivity").click(function () {
 // random button
 
 $("#submitForm").click(function () {
-    let URL = `http://www.boredapi.com/api/activity?type=${typeForm.val()}`;
-    
+  let price = priceForm.val();
+  let priceVal = "";
+  let randomNum = 0;
+  let lowArr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8];
+  console.log(price);
+  if (price == "free") {
+    priceVal = free;
+    price = 0.0;
+  } else if (price == "low") {
+    //   generate random number to select a value from low prices array
+    priceVal = "low";
+    randomNum = Math.floor(Math.random() * 3);
+    // from 0 to 2 index
+    price = lowArr[randomNum];
+  } else if (price == "medium") {
+      priceVal = "medium"
+    //   3 to 4 index
+    randomNum = Math.floor(Math.random() * 2) + 3;
+    price = lowArr[randomNum];
+  } else {
+    randomNum = Math.floor(Math.random() * 3) + 5;
+    price = lowArr[randomNum];
+    priceVal = "high"
+  }
 
-  console.log();
+  let URL = `http://www.boredapi.com/api/activity?type=${typeForm.val()}&price=${price}&participants=${peopleForm.val()}`;
+
   console.log(peopleForm.val());
-  console.log(priceForm.val());
-
 
   $.ajax({
     url: URL,
   }).then(
     (data) => {
-      $("#typeData").text(`Type: ${data.type}`)
+      $("#typeData").text(`Type: ${data.type}`);
       $("#activityData").text(`Activity: ${data.activity}`);
+      $("#priceData").text(`Price: ${priceVal}`);
     },
     (error) => {
       console.log("bad request", error);
